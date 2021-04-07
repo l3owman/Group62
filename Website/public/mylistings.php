a2ffa7aefd2d69f39382bb145996129d069e01e9
l3owman <luke_bowman@live.co.uk>
2021-04-07 16:11:32 +0100

    my listing changes

---
+++ Website/public/mylistings.php
@@ -0,0 +1,28 @@
+<?php
+include('config.php');
+session_start();
+$target_dir = "images/";
+$target_file = $target_dir . basename($_FILES["firstImage"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["firstImage"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+  if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["firstImage"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["firstImage"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
