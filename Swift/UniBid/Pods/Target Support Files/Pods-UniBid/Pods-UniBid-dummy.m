7abed6c25f9f680e383dcc1f9fb67ba3fdc865d7
mariobyebye <81801295+mariobyebye@users.noreply.github.com>
2021-04-30 17:01:30 +0300

    Add auction improvements

---
+++ Swift/UniBid/Pods/Target Support Files/Pods-UniBid/Pods-UniBid-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UniBid : NSObject
+@end
+@implementation PodsDummy_Pods_UniBid
+@end
--- Swift/UniBid/UniBid/addAuctionView.swift
+++ Swift/UniBid/UniBid/addAuctionView.swift
@@ -6,9 +6,17 @@
 //
 
 import UIKit
+import Photos
+import BSImagePicker
 
-class addAuctionView: UIViewController, UITextFieldDelegate, UITextViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
+class addAuctionView: UIViewController, UITextFieldDelegate, UITextViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    private var collectionView: UICollectionView!
+    private let identifier: String = "identifier"
+    private var selectedImages: [UIImage] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +36,31 @@ class addAuctionView: UIViewController, UITextFieldDelegate, UITextViewDelegate,
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         view.addGestureRecognizer(tap) // Add gesture recognizer to background view
+        
+        
+        let btnSelectImage: UIButton = UIButton(frame: CGRect(x: 30, y: 606, width: view.frame.size.width - 60, height: 30))
+               btnSelectImage.setTitle("Select images", for: .normal)
+               btnSelectImage.setTitleColor(UIColor.systemBlue, for: .normal)
+               btnSelectImage.contentHorizontalAlignment = .left
+               btnSelectImage.addTarget(self, action: #selector(selectImages), for: .touchUpInside)
+               view.addSubview(btnSelectImage)
+        
+        let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumLineSpacing = 10
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.minimumInteritemSpacing = 10
+        flowLayout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        flowLayout.itemSize = CGSize(width: 300, height: 300)
+ 
+        
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: btnSelectImage.frame.origin.y + btnSelectImage.frame.size.height + 10, width: view.frame.size.width, height: 115), collectionViewLayout: flowLayout)
+                collectionView.dataSource = self
+                collectionView.delegate = self
+                collectionView.backgroundColor = UIColor.clear.withAlphaComponent(0)
+                collectionView.alwaysBounceHorizontal = true
+                collectionView.register(ImageCell.self, forCellWithReuseIdentifier: identifier)
+                view.addSubview(collectionView)
+        
     }
     
     let categories = ["1A","21","312","ASD"]
@@ -79,7 +112,7 @@ class addAuctionView: UIViewController, UITextFieldDelegate, UITextViewDelegate,
             let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
 
             // make sure the listing title is under 150 characters
-            return updatedText.count <= 150
+            return updatedText.count <= 25
         } else if textField == listingBidPrice || textField == listingBuyNowPrice { // make sure we have only two digits after the . for the prices
             guard let oldText = textField.text, let r = Range(range, in: oldText) else {
                     return true
@@ -149,16 +182,76 @@ class addAuctionView: UIViewController, UITextFieldDelegate, UITextViewDelegate,
             let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
 
             // make sure the description of a listing is under 1000 characters
-            return updatedText.count <= 1000
+            return updatedText.count <= 300
         } else {
             return true
         }
     }
     
     func textViewDidChange(_ textView: UITextView) {
-        charactersLeft.text = "\(textView.text.count)/1000"
+        charactersLeft.text = "\(textView.text.count)/300"
     }
     
+    @objc private func selectImages(sender: UITapGestureRecognizer) {
+        let imagePicker = ImagePickerController()
+        //imagePicker.settings.fetch.assets.supportedMediaTypes = [.image(max: 2), .video(max: 1)]
+        presentImagePicker(imagePicker, select: { (asset) in
+        }, deselect: { (asset) in
+             
+        }, cancel: { (assets) in
+             
+        }, finish: { (assets) in
+             
+            self.selectedImages = []
+            let options: PHImageRequestOptions = PHImageRequestOptions()
+            options.deliveryMode = .highQualityFormat
+ 
+            for asset in assets {
+                PHImageManager.default().requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: options) { (image, info) in
+                    self.selectedImages.append(image!)
+                    self.collectionView.reloadData()
+                }
+            }
+        })
+    }
+     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if(selectedImages.count > 5){
+            return 5
+        }
+        return selectedImages.count
+    }
+     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let data: UIImage = selectedImages[indexPath.item]
+        let cell: ImageCell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! ImageCell
+        cell.image.image = data
+        return cell
+    }
+ 
+ 
+}
+ 
+class ImageCell: UICollectionViewCell {
+    var image: UIImageView!
+ 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+     
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+    }
+     
+    private func setupViews() {
+        image = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        image.clipsToBounds = true
+        image.contentMode = .scaleAspectFill
+        addSubview(image)
+    }
+
     
     /*
     // MARK: - Navigation
